4380e05a716ea7a0fc138e55036741e9c61d6de8
mmaluleka023 <mmaluleka023@student.wethinkcode.co.za>
2024-09-16 17:27:24 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// script.js
+document.getElementById('studentLoginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Student login functionality is not implemented.');
+});
+
+document.getElementById('sponsorLoginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Sponsor login functionality is not implemented.');
+});
